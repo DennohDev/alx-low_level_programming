583e44d11e0a32b2f04780fff9375cfe1b64900f
DennohDev <dennismaina1024@gmail.com>
2023-02-16 12:27:11 +0000

    c program to print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints a string value
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a mutlingual puzzle");
+
+	return (0);
+}
