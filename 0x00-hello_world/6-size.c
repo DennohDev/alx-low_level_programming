8a843db776eed69af020419459b8f45c5929a975
DennohDev <dennismaina1024@gmail.com>
2023-02-16 13:46:16 +0000

    Program to check the size of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - program to print the size of data types
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char chartype;
+	int inttype;
+	long int longinttype;
+	long long int longlonginttype;
+	float floattype;
+
+	printf("Size of a char: %c byte(s)\n", chartype);
+	printf("Size of an int: %d byte(s)\n", inttype);
+	printf("Size of a long int: %ld byte(s)\n", longinttype);
+	printf("Size of a long long int: %lld byte(s)\n", longlonginttype);
+	printf("Size of a float: %f byte(s)\n", floattype);
+
+	return (0);
+}
+
