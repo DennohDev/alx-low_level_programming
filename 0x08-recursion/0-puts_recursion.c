56cafb5e120fe1fa87de7bc1418883e040d643b6
DennohDev <dennismaina1024@gmail.com>
2023-03-08 07:46:52 +0000

    betty coding style and documentation fix on 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,11 +2,11 @@
 
 /**
  * _puts_recursion - prints a string followed by a new line.
- * @s pointer to a string to be printed
+ * @s: pointer to a string to be printed
  */
 void _puts_recursion(char *s)
 {
-	if(*s != '\0') 
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
